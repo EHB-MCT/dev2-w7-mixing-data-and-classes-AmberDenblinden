4315269169d2d8e88fbfccbfe36d0f138f86b642
Amber Denblinden <amber.denblinden@student.ehb.be>
2025-04-10 12:31:34 +0200

    step 1

---
+++ js/Place.js
@@ -0,0 +1,7 @@
+export default class Place {
+	constructor(adress, postalcode, municipality) {
+		this._adress = adress;
+		this._postalcode = postalcode;
+		this._municipality = municipality;
+	}
+}
